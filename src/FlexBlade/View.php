0eef5e35617c901526d570d1b063206bbd6d7271
Jack Harris <st_tuff@icloud.com>
2025-06-23 11:19:26 +1000

    - Depreciated the view bag, it now wraps Blade::Bag and will be removed in a later version.

--- src/FlexBlade/View.php
+++ src/FlexBlade/View.php
@@ -2,16 +2,23 @@
 
 namespace FlexBlade;
 
+/**
+ * @deprecated This class is deprecated and will be removed in a future version.
+ *             Use Blade::Bag() directly instead of View::Bag().
+ */
 class View
 {
-    private static Bag $bag;
-
+    /**
+     * @deprecated Use Blade::Bag() directly instead.
+     */
     public static function Bag(): Bag
     {
-        if(!isset(self::$bag)){
-            self::$bag = new Bag();
-        }
-        return self::$bag;
-    }
+        // Trigger deprecation notice
+        @trigger_error(
+            'FlexBlade\View::Bag() is deprecated. Use Blade::Bag() directly instead.',
+            E_USER_DEPRECATED
+        );
 
+        return Blade::Bag();
+    }
 }
